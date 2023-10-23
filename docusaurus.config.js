3c80252a5208c57f984a8bb3a94f655b27f1571e
Ilya Semerhanov <ilya.semerhanov@chirpwireless.io>
2023-10-23 19:30:44 +0200

    TASK-390 added GA

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,6 +33,15 @@ const config = {
       }),
     ],
   ],
+  plugins: [
+    [
+      '@docusaurus/plugin-google-gtag',
+      {
+        trackingID: 'G-ML0YTPFH70',
+        anonymizeIP: true,
+      },
+    ],
+  ],
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
